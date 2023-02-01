cdb66f850600a737e5e40f432606a785e22e73bd
David Dv Schmidt <david.dv.schmidt@deutschebahn.com>
2023-02-01 10:47:29 +0100

    update

--- requests_cache_test/test.py
+++ requests_cache_test/test.py
@@ -1,6 +1,7 @@
 import time
 
 import pytest
+from requests import Session
 from requests_cache import CachedSession
 from requests_mock import Adapter, ANY
 
@@ -9,42 +10,58 @@ MOCK_PROTOCOL = "http+mock://"
 MOCK_URL = MOCK_PROTOCOL + "test.de"
 
 
-def mount_mock_apater(session: CachedSession) -> CachedSession:
-    adapter = get_mock_adapter()
-    session.mount(MOCK_PROTOCOL, adapter)
-    session.mock_adapter = adapter
-    return session
-
-
-def get_mock_adapter() -> Adapter:
+def mount_mock_adapter(session):
     adapter = Adapter()
+    # A call on MOCK_URL should return 200 on the first call and 404 on all following calls.
     adapter.register_uri(ANY, MOCK_URL,
                          [{"text": "200 Response", "status_code": 200},
                           {"text": "404 Response", "status_code": 404}])
-    return adapter
+    session.mount(MOCK_PROTOCOL, adapter)
+    return session
 
 
 @pytest.fixture
-def mocked_session():
-    cs = CachedSession(stale_if_error=True, expire_after=1, allowable_codes=[200, 404])
-    cs = mount_mock_apater(cs)
+def mocked_cached_session():
+    cs = CachedSession(stale_if_error=True, expire_after=5, allowable_codes=[200, 404])
+    cs = mount_mock_adapter(cs)
     yield cs
     cs.cache.clear()
 
 
+@pytest.fixture
+def mocked_session():
+    s = Session()
+    s = mount_mock_adapter(s)
+    yield s
+
+
 class TestCachedSession:
 
-    def test_200_404(self, mocked_session):
+    def test_mock_adapter(self, mocked_session):
+        """
+        Check if the mock adapter works as expected.
+        For this we use a normal session.
+        """
         result = mocked_session.get(MOCK_URL)
         assert result.status_code == 200
-        assert result.from_cache is False
-
-        result = mocked_session.get(MOCK_URL)
-        assert result.status_code == 200
-        assert result.from_cache is True
-
-        time.sleep(2)
-
         result = mocked_session.get(MOCK_URL)
         assert result.status_code == 404
-        assert result.from_cache is False
+        result = mocked_session.get(MOCK_URL)
+        assert result.status_code == 404
+
+    def test_200_404(self, mocked_cached_session):
+        result = mocked_cached_session.get(MOCK_URL)  # as the cache is new, a call to MOCK_URL is made, which returns 200
+        assert result.status_code == 200
+        assert result.from_cache is False  # the result is "fresh"
+        assert result.is_expired is False  # the result is "fresh"
+
+        result = mocked_cached_session.get(MOCK_URL)  # as the cache for this request is not expired yet, no new call is made and the cache is returned
+        assert result.status_code == 200
+        assert result.from_cache is True
+        assert result.is_expired is False
+
+        time.sleep(6)  # wait for the cache to expire
+
+        result = mocked_cached_session.get(MOCK_URL)  # as the cache is expired, a new call is made, which returns 404
+        assert result.status_code == 404  # 404 should be passed through, as it's in allowable_codes
+        assert result.is_expired is False  # The result should not be expired, as the 404 is "fresh"
